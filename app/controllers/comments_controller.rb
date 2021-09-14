32eb9419d67a6fdb4ebdad89bf088490951f88e9
mae30726 <mae30726@gmail.com>
2021-09-14 11:22:25 +0900

    コメントフォームのエラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to "/prototypes/#{@comment.prototype.id}"
     else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
